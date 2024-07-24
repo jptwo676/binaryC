5584394631f3fca11a0b6ed31cc06500664823d6
jptwo <joaobtwp@gmail.com>
2024-07-23 22:00:30 -0300

    23/07

--- main.c
+++ main.c
@@ -1,35 +1,74 @@
 #include <stdio.h>
 
-void bubbleSort(int* array);
-void imprime(int* array);
+void bubbleSort(int* array, int len);
+void imprime(int* array, int len);
+int binarySearch(int* array, int alvo, int inicio, int final);
 
 int main()
 {
+    int numAlvo;
+    int bResult;
 
     int numeros[6] = {60, 50, 25, 10, 2, 1002};
+    int numerosLen = sizeof(numeros)/sizeof(numeros[0]);
 
-    for(int i = 0; i < 6; i++)
-    {
-        printf("%d ", numeros[i]);
-    }
-
-    printf("\n");
-
+    printf("Antes do bubble sort: \n");
+    imprime(numeros, numerosLen);
                 
     printf("Após bubble sort: \n");
-    bubbleSort(numeros);
-    imprime(numeros);
+    bubbleSort(numeros, numerosLen);
+    imprime(numeros, numerosLen);
+
+    printf("Digite um numero p/ ser procurado: \n");
+    scanf("%d", &numAlvo); 
+
+    bResult = binarySearch(numeros, numAlvo, 0, 5);
+
+    if(bResult == 1)
+    {
+        printf("Numero não encontrado. \n");
+    }
+    else
+    {
+        printf("Valor encontrado. \n");
+    }
 
     return 0;
 }
 
-void bubbleSort(int* array)
+int binarySearch(int* array, int alvo, int inicio, int final)
+{
+    int meio;
+
+    //TODO n ta achando o 60
+    while(inicio <= final)
+    {
+        meio = (inicio + final) / 2;
+        if(alvo > array[meio])
+        {
+            inicio = meio + 1;
+        }
+        else if(alvo < array[meio])
+        {
+            final = meio - 1;
+        }
+        else
+        {
+            return meio;
+        }
+    }
+
+    return 1;
+}
+
+//TODO ordernar crescente (ta no decrecente)
+void bubbleSort(int* array, int len)
 {
     int aux = 0;
 
-    for(int i = 0; i < 6; i++)
+    for(int i = 0; i < len; i++)
     {
-        for(int j = 0; j < 6; j++)
+        for(int j = 0; j < len; j++)
         {
             if(array[i] > array[j])
             {
@@ -41,9 +80,9 @@ void bubbleSort(int* array)
     }
 }
 
-void imprime(int* array)
+void imprime(int* array, int len)
 {
-    for(int i = 0; i < 6; i++)
+    for(int i = 0; i < len; i++)
     {
         printf("%d ", array[i]);
     }
